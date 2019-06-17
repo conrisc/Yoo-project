9ca09f39088d4eead90c5067eff6d43991361309
Konrad Cielecki <Konrad.Cielecki@ad.infosys.com>
2019-06-17 09:47:56 +0200

    Initial commit from Create React App

---
+++ client/src/Content.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Content extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    alert() {
+        alert("lol");
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Sign in</h3>
+                <form onSubmit={this.alert} >
+                    <div class="form-group"> 
+                        <input type="text" class="form-control" placeholder="login"/>
+                    </div>
+                    <div class="form-group"> 
+                        <input type="password" class="form-control" placeholder="password"/>
+                    </div>
+                    <input type="submit" value="Sign in" class="btn btn-primary"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export {
+    Content
+}
