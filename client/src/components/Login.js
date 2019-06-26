37b079e2023040b2288117e227814ca22057680f
Konrad C <conrisc@gmail.com>
2019-06-26 13:16:23 +0200

    Prepare LoginService

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { loginService } from '../services/';
 
 class Login extends React.Component {
     constructor(props) {
@@ -23,7 +24,8 @@ class Login extends React.Component {
             login: this.state.login,
             password: this.state.password
         }
-        return postData('http://localhost:3001/signin', credentials)
+
+        loginService.signIn(credentials)
             .then(msg => {
                 if (msg.login === 'success')
                     this.props.history.push('/panel');
@@ -32,24 +34,7 @@ class Login extends React.Component {
             })
             .catch(err => {
                 this.setState({ errorMsg: err.message });
-            })
-
-            function postData(url = '', data = {}) {
-                return fetch(url, {
-                    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                    mode: 'cors', // no-cors, cors, *same-origin
-                    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                    credentials: 'same-origin', // include, *same-origin, omit
-                    headers: {
-                        'Content-Type': 'application/json',
-                        // 'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    redirect: 'follow', // manual, *follow, error
-                    referrer: 'no-referrer', // no-referrer, *client
-                    body: JSON.stringify(data), // body data type must match "Content-Type" header
-                })
-                .then(response => response.json()); // parses JSON response into native JavaScript objects 
-            }
+            });
     }
 
     render() {
