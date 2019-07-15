137f19e042e80473b11ae748310f7bb328adc287
Konrad C <conrisc@gmail.com>
2019-07-15 13:18:25 +0200

    Add google map

--- client/src/components/Trips.tsx
+++ client/src/components/Trips.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
+// @ts-ignore
+const google = window.google;
+
 class Trips extends React.Component {
+
+    initMap() {
+    }
+
+    componentDidMount() {
+        let map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+    }
+
     render() {
         return (
             <div>
-                This is your trips
+                <div id="map"></div>
             </div>
         );
     }
