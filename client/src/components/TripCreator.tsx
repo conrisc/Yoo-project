cefda3bc3d921c0a296df787119518190c4c43ba
Konrad C <conrisc@gmail.com>
2019-07-25 14:33:25 +0200

    Add directions API and slide animation

--- client/src/components/TripCreator.tsx
+++ client/src/components/TripCreator.tsx
@@ -1,18 +1,78 @@
 import React from 'react';
 
+const TOKEN = "AIzaSyBvvydrMAOa6nn4vH-HJgV_Z9Ep-v7QHfk";
+
 // @ts-ignore
 const google = window.google;
 
 class TripCreator extends React.Component {
+    readonly state;
+    map;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            startingPoint: '',
+            destinationPoint: '',
+            transportTypeClass: 'hide'
+        }
+    }
 
     initMap() {
     }
 
     componentDidMount() {
-        let map = new google.maps.Map(document.getElementById('map'), {
+        this.map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 48, lng: 17},
             zoom: 4
         });
+    }
+
+    startpointChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    find(event) {
+        event.preventDefault();
+
+        const directionsDisplay = new google.maps.DirectionsRenderer;
+        directionsDisplay.setMap(this.map);
+        const directionService = new google.maps.DirectionsService;
+        directionService.route({
+            origin: this.state.startingPoint,
+            destination: this.state.destinationPoint,
+            travelMode: 'DRIVING'
+            }, (response, status) => {
+            console.log(response);
+            console.log(response.routes[0].legs[0].end_location.lat());
+            var flightPlanCoordinates = [
+                {lat: response.routes[0].legs[0].start_location.lat(), lng: response.routes[0].legs[0].start_location.lng()},
+                {lat: response.routes[0].legs[0].end_location.lat(), lng: response.routes[0].legs[0].end_location.lng()},
+            ];
+            var flightPath = new google.maps.Polyline({
+                path: flightPlanCoordinates,
+                geodesic: true,
+                strokeColor: '#FF0000',
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+            });
+
+            flightPath.setMap(this.map);
+            if (status === 'OK') {
+                directionsDisplay.setDirections(response);
+            } else {
+                window.alert('Directions request failed due to ' + status);
+            }
+        });
+    }
+
+    handleTransportTypeChange(event) {
+        this.setState({
+            transportTypeClass: event.target.value === 'PROVIDED' ? 'show' : 'hide'
+        }) 
     }
 
     render() {
@@ -23,17 +83,32 @@ class TripCreator extends React.Component {
                     <form className="col simple-form">
                         <div className="form-group">
                             <label className="col-form-label col-form-label-sm" htmlFor="inputStart">Starting point</label>
-                            <input className="form-control form-control-sm" id="inputStart" type="text" placeholder="What's your starting point?"/>
+                            <input className="form-control form-control-sm" id="inputStart" 
+                            type="text" placeholder="What's your starting point?" name="startingPoint" onChange={e => this.startpointChange(e)}/>
                         </div>
                         <div className="form-group">
                             <label className="col-form-label col-form-label-sm" htmlFor="inputDestination">Destination</label>
-                            <input className="form-control form-control-sm" id="inputDestination" type="text" placeholder="Where would you like to go?" />
+                            <input className="form-control form-control-sm" id="inputDestination" type="text" placeholder="Where would you like to go?"
+                            name="destinationPoint" onChange={e => this.startpointChange(e)} />
                         </div>
+                        <button onClick={(e) => this.find(e)}>Find</button>
                         <div className="form-group">
-                            <label className="col-form-label col-form-label-sm" htmlFor="inputTransport">Transport</label>
-                            <select id="inputTransport" className="form-control form-control-sm">
-                                <option defaultValue="true">On your own</option>
-                                <option>Car</option>
+                            <label className="col-form-label col-form-label-sm">Transport</label><br />
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="transportType" id="ownTransport" value="OWN"
+                                onChange={e=> this.handleTransportTypeChange(e)}/>
+                                <label className="form-check-label col-form-label-sm" htmlFor="ownTransport">Everyone on its own</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="transportType" id="providedTransport" value="PROVIDED"
+                                onChange={e=> this.handleTransportTypeChange(e)}/>
+                                <label className="form-check-label col-form-label-sm" htmlFor="providedTransport">Transport provided</label>
+                            </div>
+                        </div>
+                        <div className={'form-group ' + this.state.transportTypeClass}>
+                            <label className="col-form-label col-form-label-sm" htmlFor="selectTransportType">Type of transport</label>
+                            <select id="selectTransportType" className="form-control form-control-sm">
+                                <option defaultValue="true">Car</option>
                                 <option>Bus</option>
                                 <option>Plane</option>
                             </select>
