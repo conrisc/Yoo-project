5d3d6409cfe77e8604c151f66d9a739bccdc7efa
Konrad C <conrisc@gmail.com>
2019-07-25 16:23:37 +0200

    Add data handling

--- client/src/components/TripCreator.tsx
+++ client/src/components/TripCreator.tsx
@@ -8,34 +8,41 @@ const google = window.google;
 class TripCreator extends React.Component {
     readonly state;
     map;
+    mapRef;
 
     constructor(props) {
         super(props);
 
+        this.mapRef = React.createRef();
         this.state = {
             startingPoint: '',
             destinationPoint: '',
+            transport: 'own',
+            transportType: 'car',
+            numberOfPeople: 1,
+            accommodation: 'own',
+            startDate: '',
+            endDate: '',
+            tripDescrpition: '',
             transportTypeClass: 'hide'
         }
     }
 
-    initMap() {
-    }
-
     componentDidMount() {
-        this.map = new google.maps.Map(document.getElementById('map'), {
+        this.map = new google.maps.Map(this.mapRef.current, {
             center: {lat: 48, lng: 17},
             zoom: 4
         });
     }
 
-    startpointChange(event) {
+    handleInputChange(event) {
+        console.log(event.target.name, event.target.value);
         this.setState({
             [event.target.name]: event.target.value
         });
     }
 
-    find(event) {
+    findDirection(event) {
         event.preventDefault();
 
         const directionsDisplay = new google.maps.DirectionsRenderer;
@@ -71,7 +78,7 @@ class TripCreator extends React.Component {
 
     handleTransportTypeChange(event) {
         this.setState({
-            transportTypeClass: event.target.value === 'PROVIDED' ? 'show' : 'hide'
+            transportTypeClass: event.target.value === 'provided' ? 'show' : 'hide'
         }) 
     }
 
@@ -79,60 +86,62 @@ class TripCreator extends React.Component {
         return (
             <div>
                 <div className="row py-2">
-                    <div id="map" className="col-9"></div>
+                    <div ref={this.mapRef} className="col-9"></div>
                     <form className="col simple-form">
                         <div className="form-group">
                             <label className="col-form-label col-form-label-sm" htmlFor="inputStart">Starting point</label>
                             <input className="form-control form-control-sm" id="inputStart" 
-                            type="text" placeholder="What's your starting point?" name="startingPoint" onChange={e => this.startpointChange(e)}/>
+                            type="text" placeholder="What's your starting point?" name="startingPoint" onChange={e => this.handleInputChange(e)}/>
                         </div>
                         <div className="form-group">
                             <label className="col-form-label col-form-label-sm" htmlFor="inputDestination">Destination</label>
                             <input className="form-control form-control-sm" id="inputDestination" type="text" placeholder="Where would you like to go?"
-                            name="destinationPoint" onChange={e => this.startpointChange(e)} />
+                            name="destinationPoint" onChange={e => this.handleInputChange(e)} />
                         </div>
-                        <button onClick={(e) => this.find(e)}>Find</button>
+                        <button onClick={e => this.findDirection(e)}>Find</button>
                         <div className="form-group">
                             <label className="col-form-label col-form-label-sm">Transport</label><br />
                             <div className="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" name="transportType" id="ownTransport" value="OWN"
-                                onChange={e=> this.handleTransportTypeChange(e)}/>
+                                <input className="form-check-input" type="radio" name="transport" id="ownTransport" value="own"
+                                onChange={e=> { this.handleTransportTypeChange(e); this.handleInputChange(e) }}/>
                                 <label className="form-check-label col-form-label-sm" htmlFor="ownTransport">Everyone on its own</label>
                             </div>
                             <div className="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" name="transportType" id="providedTransport" value="PROVIDED"
-                                onChange={e=> this.handleTransportTypeChange(e)}/>
+                                <input className="form-check-input" type="radio" name="transport" id="providedTransport" value="provided"
+                                onChange={e=> { this.handleTransportTypeChange(e); this.handleInputChange(e)}}/>
                                 <label className="form-check-label col-form-label-sm" htmlFor="providedTransport">Transport provided</label>
                             </div>
                         </div>
                         <div className={'form-group ' + this.state.transportTypeClass}>
                             <label className="col-form-label col-form-label-sm" htmlFor="selectTransportType">Type of transport</label>
-                            <select id="selectTransportType" className="form-control form-control-sm">
-                                <option defaultValue="true">Car</option>
-                                <option>Bus</option>
-                                <option>Plane</option>
+                            <select id="selectTransportType" className="form-control form-control-sm" name="transportType" value={this.state.transportType}
+                                onChange={e => this.handleInputChange(e)}>
+                                <option value="car">Car</option>
+                                <option value="bus">Bus</option>
+                                <option value="plane">Plane</option>
                             </select>
                         </div>
                         <div className="form-group">
                             <label className="col-form-label col-form-label-sm" htmlFor="inputNumberOfPeople">Number of people</label>
-                            <input className="form-control form-control-sm" id="inputNumberOfPeople" type="number" defaultValue="1" min="1" />
+                            <input className="form-control form-control-sm" id="inputNumberOfPeople" type="number"
+                            value={this.state.numberOfPeople} name="numberOfPeople" min="1" max="20" onChange={e => this.handleInputChange(e)}/>
                             <small className="form-text text-muted">Do not include yourself</small>
                         </div>
                         <div className="form-group">
                             <label className="col-form-label col-form-label-sm" htmlFor="inputAccommodation">Accommodation</label>
-                            <select id="inputAccommodation" className="form-control form-control-sm">
-                                <option defaultValue="true">Provided</option>
-                                <option>On your own</option>
+                            <select id="inputAccommodation" className="form-control form-control-sm" name="accommodation" onChange={e => this.handleInputChange(e)}>
+                                <option value="provided">Provided</option>
+                                <option value="own">On your own</option>
                             </select>
                         </div>
                         <div className="row">
                             <div className="form-group col w-50">
                                 <label className="col-form-label col-form-label-sm" htmlFor="inputDeparture">Date of departure</label>
-                                <input id="inputDeparture" className="form-control form-control-sm" type="date"/>
+                                <input id="inputDeparture" className="form-control form-control-sm" type="date" name="startDate" onChange={e => this.handleInputChange(e)} />
                             </div>
                             <div className="form-group col w-50">
                                 <label className="col-form-label col-form-label-sm" htmlFor="inputReturn">Date of return</label>
-                                <input id="inputReturn" className="form-control form-control-sm" type="date"/>
+                                <input id="inputReturn" className="form-control form-control-sm" type="date" name="endDate" onChange={e => this.handleInputChange(e)} />
                             </div>
                         </div>
                     </form>
@@ -140,7 +149,8 @@ class TripCreator extends React.Component {
                 <div className="row mt-2">
                     <form className="col simple-form">
                         <div className="form-group">
-                            <textarea className="form-control form-control-sm" placeholder="Description of your trip">
+                            <textarea className="form-control form-control-sm" placeholder="Description of your trip" name="tripDescription"
+                                onChange={e => this.handleInputChange(e)}>
                             </textarea>
                         </div>
                     </form>
